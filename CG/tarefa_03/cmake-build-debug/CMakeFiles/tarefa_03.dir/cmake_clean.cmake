30b4bb4e8f080a6182a4e385d4f05b1c77672d1d
Willian Zocolau <willianzocolau@outlook.com>
2019-09-10 22:22:11 -0300

    init atv 03

---
+++ CG/tarefa_03/cmake-build-debug/CMakeFiles/tarefa_03.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tarefa_03.dir/main.cpp.o"
+  "tarefa_03.pdb"
+  "tarefa_03"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tarefa_03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
